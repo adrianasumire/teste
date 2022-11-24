bad3b1d2dd48e0088234655aef13e2e43ae44810
adrianasumire <adriana.sumire@gmail.com>
2022-11-24 02:42:03 -0300

    6

--- inicia_dotnet.sh
+++ inicia_dotnet.sh
@@ -1,5 +1,4 @@
 export PATH=/usr/bin:$PATH
 export DOTNET_ROOT=/usr/share/dotnet/dotnet_5_0/
-cp /app/teste/appsettings.json /app/backend-doai/DoaiApi/.
 cd /app/backend-doai/DoaiApi
 nohup dotnet run &
---
+++ prepara_backend.sh
@@ -0,0 +1,3 @@
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.orig
+mv /app/teste/nginx_backend.txt /etc/nginx/sites-available/default
+chmod 755 /etc/nginx/sites-available/default
